95e767273f43a658725a623446b6ff323520ff7f
Anil-Yadav1 <yadavanilkumar887@gmail.com>
2025-08-21 13:19:11 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+add = a+b
+subt = a-b
+mult = a*b
+div = a/b
+print("Addition: ",add)
+print("Subtraction: ",subt)
+print("Multiplication: ",mult)
+print("Division: ",div)
---
+++ program2.py
@@ -0,0 +1,4 @@
+fname = input("Enter first name: ")
+lname= input("Enter last name: ")
+funame = fname + " " + lname
+print("Hello",funame,"welcome to the python program")
